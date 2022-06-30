8cfa9e5f347dd8214cd846328fdcd8f2bb7a50b8
Oluwaseyi <seyistry@gmail.com>
2022-06-30 14:24:53 -0700

    101  quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char myString[] = "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19";
+	fprintf(stderr, myString);
+	return (1);
+}
