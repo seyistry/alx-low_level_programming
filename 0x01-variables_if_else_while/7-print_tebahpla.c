8ece9f2826ac82cb8053d12c857e1b8359c0d381
Oluwaseyi <seyistry@gmail.com>
2022-07-01 14:04:54 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i < 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
