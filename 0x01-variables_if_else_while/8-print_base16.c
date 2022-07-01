ad4fde911106f8564583c30b441a3462607b352b
Oluwaseyi <seyistry@gmail.com>
2022-07-01 14:15:09 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = '0';
+	char cha = 'a';
+	
+	while (ch < ':')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (cha < 'g')
+	{
+		putchar(cha);
+		cha++;
+	}
+	putchar('\n');
+	return (0);
+}
