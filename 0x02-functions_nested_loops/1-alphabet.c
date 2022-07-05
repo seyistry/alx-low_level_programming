e03812e134265f340de26a38be5116991ee75bc1
Oluwaseyi <seyistry@gmail.com>
2022-07-05 08:09:09 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while ( i < '{')
+	{
+		_printchar(i);
+		i++
+	}
+}
