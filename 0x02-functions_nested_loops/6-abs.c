c956eaf5c5aef16e54ce1c734774570aa064acaa
Oluwaseyi <seyistry@gmail.com>
2022-07-05 12:55:54 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - check if value C is lowercase
+ *@n: n is interger
+ *
+ *Return: return an int n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -1 * n
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
