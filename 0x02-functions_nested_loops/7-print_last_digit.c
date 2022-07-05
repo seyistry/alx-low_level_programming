8be0ecb4e01b23b74c5240aeb204ba967dfeed3b
Oluwaseyi <seyistry@gmail.com>
2022-07-05 13:03:03 -0700

    add functions that prints last digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - cprints last digit of a number 
+ *@n: n is interger
+ *
+ *Return: return the last digits
+ */
+int print_last_digit(int n);
+{
+	n = n % 10;
+	return (n);
+}
