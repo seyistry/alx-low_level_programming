4c691f6725a05a4dc32398829ede4f168c5f3974
Oluwaseyi <seyistry@gmail.com>
2022-07-05 14:28:44 -0700

    0 - 24hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - print count to 99:99
+ *@n: n is interger
+ *
+ *Return: return the last digits
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < 100)
+	{
+		j = 0;
+		while (j < 100)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(58);
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
