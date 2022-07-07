86cb24a14be5617cc44f3b56a2ae196d6bf5ac30
Oluwaseyi <seyistry@gmail.com>
2022-07-07 09:30:56 -0700

    first push

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most number
+ *
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+	int n = 48;
+	while (n < 59)
+	{
+		if (!(n == 2 || n == 4))
+		{
+			_putchar(n);
+			n++;
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
+int _putchar(char c);
 int _isupper(int c);
-
-#endif /* MAIN_H */
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
