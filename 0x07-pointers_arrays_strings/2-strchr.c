69948bab579bcece0c5131f8f0b06da7118dd279
Oluwaseyi <seyistry@gmail.com>
2022-07-18 13:26:53 -0700

    add ex 1 amd 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strchr - find a character in a string
+ * @s: string to check
+ * @c: character
+ * Return: pointer to char or return NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+	    if (c == '\0')
+		    return (s);
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	return (NULL);
+}
