5f2d1d4355f23e326a6c58fb484aa2ea4f37c348
Oluwaseyi <seyistry@gmail.com>
2022-07-18 13:43:48 -0700

    add exe

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - length of a prefix substring
+ * @s: segment
+ * @accept: bytes accepted
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\n'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+	}
+	return (c);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: char string
+ * @accept: bytes accepted
+ * Return: a 
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
