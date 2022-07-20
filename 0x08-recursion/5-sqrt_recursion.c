8a102540fe37c3b15a4998a30f7ab8c24271d3b6
Oluwaseyi <seyistry@gmail.com>
2022-07-20 07:54:42 -0700

    fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,24 @@
-include main.h
+
+#include "main.h"
+
+/**
+ * helper - square root of number
+ * @i: input number
+ * @j: iterator
+ * Return: square root or -1
+ */
+int helper(int i, int j)
+{
+	if (j % (i / j) == 0)
+	{
+		if (j * (i / j) == i)
+			return (j);
+		else
+			return (-1);
+	}
+	return (0 + helper(i, j + 1));
+}
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number
