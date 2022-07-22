d664a9dfa1c162e08fb9ed9af4ed8954a14d7669
Oluwaseyi <seyistry@gmail.com>
2022-07-22 13:34:53 -0700

    whatismyname"

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: command line arg number
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+  return 0;
+}
