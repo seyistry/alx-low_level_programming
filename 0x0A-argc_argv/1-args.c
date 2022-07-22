b905eca96af492bb9bb07d02dcb247caa3040f82
Oluwaseyi <seyistry@gmail.com>
2022-07-22 13:46:18 -0700

    total argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argc: arg
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
