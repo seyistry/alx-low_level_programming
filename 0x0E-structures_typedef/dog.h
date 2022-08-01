86ebec62a4c2ab2902fd8a138b2df9a798e3754e
Oluwaseyi <seyistry@gmail.com>
2022-08-01 05:52:36 -0700

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - Short description
+ * @name: name
+ * @age: age of type float
+ * @owner: owner
+ *
+ * Description: description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
