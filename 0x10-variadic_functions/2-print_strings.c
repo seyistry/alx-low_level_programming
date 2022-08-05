8ec39aa865dada5df4498020d9f741ac3ea77b1c
Oluwaseyi <seyistry@gmail.com>
2022-08-05 15:20:39 -0700

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers
+ * print_strings - prints numbers
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  *
