eb1a6844d7120f0518c506d5f6ab8bee941bf873
Oluwaseyi <seyistry@gmail.com>
2022-08-15 13:14:49 -0700

    fix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = (*head);
 	(*head) = new_node;
-	if (new_node == NULL)
+	if (!new_node)
 		return (NULL);
 	return (new_node);
 }
