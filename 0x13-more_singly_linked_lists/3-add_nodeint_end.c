cbc20737210c2c14be4a38acf11e93fe9d6f44ad
Oluwaseyi <seyistry@gmail.com>
2022-08-16 06:58:07 -0700

    fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,30 +10,28 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    /* 1. allocate node */
-    listint_t *new_node = malloc(sizeof(listint_t));
+	/* 1. allocate node */
+	listint_t *new_node = malloc(sizeof(listint_t));
 
-    listint_t *last = *head; /* used in step 5*/
+	listint_t *last = *head; /* used in step 5*/
 
-    /* 2. put in the data  */
-    new_node->n = n;
+	/* 2. put in the data  */
+	new_node->n = n;
 
-    /* 3. This new node is going to be the last node, so make next
-         of it as NULL*/
-    new_node->next = NULL;
+	new_node->next = NULL;
 
-    /* 4. If the Linked List is empty, then make the new node as head */
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (NULL);
-    }
+	/* 4. If the Linked List is empty, then make the new node as head */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (NULL);
+	}
 
-    /* 5. Else traverse till the last node */
-    while (last->next != NULL)
-        last = last->next;
+	/* 5. Else traverse till the last node */
+	while (last->next != NULL)
+		last = last->next;
 
-    /* 6. Change the next of last node */
-    last->next = new_node;
-    return (new_node);
+	/* 6. Change the next of last node */
+	last->next = new_node;
+	return (new_node);
 }
