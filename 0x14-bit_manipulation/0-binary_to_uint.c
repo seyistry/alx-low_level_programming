f55e891f7912fe8c03db728d44ba6b128da5fffe
Oluwaseyi <seyistry@gmail.com>
2022-08-18 18:27:04 -0700

    add exe

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,26 +8,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i;
-	int l, b_two;
+	int i;
+	unsigned int num;
 
+	num = 0;
 	if (!b)
 		return (0);
-	i = 0;
-
-	for (l = 0; b[l] != '\0'; l++)
-		;
-	for (l--, b_two = 1; l >= 0; l--, b_two *= 2)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[l] != '0' && b[l] != '1')
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-
-		if (b[l] & 1)
-		{
-			i += b_two;
-		}
 	}
-	return (i);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
 }
