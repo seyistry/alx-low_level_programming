9dfc663780abd88ad545fe599d2f75d649afb887
Oluwaseyi <seyistry@gmail.com>
2022-08-23 12:51:47 -0700

    rewrite code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,120 @@
 #include "main.h"
-
 /**
- * error_file - checks on files
- * @file_from: file from
- * @file_to: file_to
- * @argv: argument vector
+ * excode97 - checks for the correct number of arguments
+ * @argc: number of arguments
  *
  * Return: void
  */
-void error_file(int file_from, int file_to, char *argv[])
+void excode97(int argc)
 {
-	if (file_from == -1)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 }
 
 /**
- * main - check code
- * @argc: number of argument
- * @argv: arguments of vector
+ * excode98 - checks that file_from exists and can be read
+ * @check: checks if true of false
+ * @file: file_from name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
  *
- * Return: 0
+ * Return: void
+ */
+void excode98(long check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(98);
+	}
+}
+/**
+ * excode99 - checks that file_to was created and/or can be written to
+ * @check: checks if true of false
+ * @file: file_to name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void excode99(long check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(99);
+	}
+}
+/**
+ * excode100 - checks that file descriptors were closed properly
+ * @check: checks if true or false
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void excode100(int check, int fd)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * main - copies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	int fild_from, fild_to, close_to, close_from;
+	char buffer[1024];
+	long lenr, lenw;
+	int file_permission;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+	/*checks number of arguments*/
+	excode97(argc);
 
-	nchars = 1024;
-	while (nchars == 1024)
+	fild_from = open(argv[1], O_RDONLY);
+
+	/*checks if files exists and can be read, code98*/
+	excode98(fild_from, argv[1], -1, -1);
+	file_permission = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fild_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_permission);
+	/*checks if file exists and ca be read, code99*/
+	excode99(fild_to, argv[2], fild_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
+		/* here we read and write*/
+		lenr = read(fild_from, buffer, 1024);
+		excode98(lenr, argv[1], fild_from, fild_to);
+		lenw = write(fild_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+
+		excode99(lenw, argv[2], fild_from, fild_to);
 	}
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+		/*close part*/
+
+		close_to = close(fild_to);
+		close_from = close(fild_from);
+
+		excode100(close_to, fild_to);
+		 excode100(close_from, fild_from);
+
 	return (0);
 }
