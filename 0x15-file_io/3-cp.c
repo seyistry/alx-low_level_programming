f5d8e82ab22b1fd9d7b4405106cea33b46aecf47
Oluwaseyi <seyistry@gmail.com>
2022-08-23 13:00:27 -0700

    change varaible names

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * excode97 - checks for the correct number of arguments
  * @argc: number of arguments
@@ -9,7 +10,7 @@ void excode97(int argc)
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from cp_to\n");
 		exit(97);
 	}
 }
@@ -19,7 +20,7 @@ void excode97(int argc)
  * @check: checks if true of false
  * @file: file_from name
  * @fd_from: file descriptor of file_from, or -1
- * @fd_to: file descriptor of file_to, or -1
+ * @fd_to: file descriptor of cp_to, or -1
  *
  * Return: void
  */
@@ -36,11 +37,11 @@ void excode98(long check, char *file, int fd_from, int fd_to)
 	}
 }
 /**
- * excode99 - checks that file_to was created and/or can be written to
+ * excode99 - checks that cp_to was created and/or can be written to
  * @check: checks if true of false
- * @file: file_to name
+ * @file: cp_to name
  * @fd_from: file descriptor of file_from, or -1
- * @fd_to: file descriptor of file_to, or -1
+ * @fd_to: file descriptor of cp_to, or -1
  *
  * Return: void
  */
@@ -80,7 +81,7 @@ void excode100(int check, int fd)
  */
 int main(int argc, char *argv[])
 {
-	int fild_from, fild_to, close_to, close_from;
+	int cp_from, cp_to, close_to, close_from;
 	char buffer[1024];
 	long lenr, lenw;
 	int file_permission;
@@ -88,33 +89,33 @@ int main(int argc, char *argv[])
 	/*checks number of arguments*/
 	excode97(argc);
 
-	fild_from = open(argv[1], O_RDONLY);
+	cp_from = open(argv[1], O_RDONLY);
 
 	/*checks if files exists and can be read, code98*/
-	excode98(fild_from, argv[1], -1, -1);
+	excode98(cp_from, argv[1], -1, -1);
 	file_permission = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fild_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_permission);
+	cp_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_permission);
 	/*checks if file exists and ca be read, code99*/
-	excode99(fild_to, argv[2], fild_from, -1);
+	excode99(cp_to, argv[2], cp_from, -1);
 	lenr = 1024;
 	while (lenr == 1024)
 	{
 		/* here we read and write*/
-		lenr = read(fild_from, buffer, 1024);
-		excode98(lenr, argv[1], fild_from, fild_to);
-		lenw = write(fild_to, buffer, lenr);
+		lenr = read(cp_from, buffer, 1024);
+		excode98(lenr, argv[1], cp_from, cp_to);
+		lenw = write(cp_to, buffer, lenr);
 		if (lenw != lenr)
 			lenw = -1;
 
-		excode99(lenw, argv[2], fild_from, fild_to);
+		excode99(lenw, argv[2], cp_from, cp_to);
 	}
 		/*close part*/
 
-		close_to = close(fild_to);
-		close_from = close(fild_from);
+		close_to = close(cp_to);
+		close_from = close(cp_from);
 
-		excode100(close_to, fild_to);
-		 excode100(close_from, fild_from);
+		excode100(close_to, cp_to);
+		 excode100(close_from, cp_from);
 
 	return (0);
 }
