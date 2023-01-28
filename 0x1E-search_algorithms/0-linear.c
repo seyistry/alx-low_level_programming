4a1abbfcf70e773fd1f66f8f4c04456da040b503
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-01-28 22:38:08 +0100

    setup

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array
+ * @size: is the number of elements in array
+ * @array: is a pointer to the first element of the array to search
+ * @value: is the value to search
+ * Return: return the first index where value is located
+ */
+int	linear_search(int *array, size_t size, int value)
+{
+	return (0);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef _HEADER_
+#define _HEADER_
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/* prototypes*/
+int linear_search(int *array, size_t size, int value);
